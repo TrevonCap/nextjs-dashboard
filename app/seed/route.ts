b41731d32203363f1972c82a4c8a6998ad733ba5
TrevonCap <126960376+TrevonCap@users.noreply.github.com>
2024-11-02 16:06:14 +0100

    Update route.ts

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -106,13 +106,13 @@ export async function GET() {
  //   message:
    //   'Uncomment this file and remove this line. You can delete this file when you are finished.',
  // });
-   try {
-     await client.sql`BEGIN`;
-     await seedUsers();
-     await seedCustomers();
-     await seedInvoices();
-     await seedRevenue();
-     await client.sql`COMMIT`;
+//   try {
+  //   await client.sql`BEGIN`;
+   //  await seedUsers();
+   //  await seedCustomers();
+   //  await seedInvoices();
+   //  await seedRevenue();
+   //  await client.sql`COMMIT`;
 
      return Response.json({ message: 'Database seeded successfully' });
    } catch (error) {
